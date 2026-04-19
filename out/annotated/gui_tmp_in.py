e27bd8cee8b889cf3cd996a4595df0f7a2904364
rishi-2406 <ladrishi24@gmail.com>
2026-04-19 14:02:51 +0530

    GLM5.1-magic

--- out/annotated/gui_tmp_in.py
+++
@@ -1,8 +0,0 @@
-def run_unsafe_code(user_input):
-    eval(user_input)
-    password = "supersecretpassword123"
-    print("Doing something with ", password)
-
-
-def standard_function(a: int, b: int) -> int:
-    return a + b
